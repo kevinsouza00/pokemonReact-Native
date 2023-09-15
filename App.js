617f322e6ec169f4ee524191281a6c3c925f7d63
Kevin.Souza <114522453+kevinsouza00@users.noreply.github.com>
2023-09-14 23:03:07 -0300

    finalizar quadro de desenho

--- App.js
+++ App.js
@@ -3,17 +3,23 @@ import Form from './src/screens/Form';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import PokemonList from './src/screens/PokemonList';
-import NewAPIScreen from './src/screens/NewAPIScreen';
+import NewAPIScreen from './src/screens/PaginaDetalhesPokemon';
+import { Text } from 'react-native';
+import PaginaDetalhesPokemon from './src/screens/PaginaDetalhesPokemon';
+import Desenhador from './src/screens/desenhador';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Form" component={Form} />
         <Stack.Screen name="Pokemons" component={PokemonList} />
-        <Stack.Screen name="NewAPI" component={NewAPIScreen} /> {/* Adicione esta linha */}
+        <Stack.Screen name="DetalhePokemon" component={PaginaDetalhesPokemon} />
+        <Stack.Screen name="Mostre seu talento no desenho" component={Desenhador} />
       </Stack.Navigator>
     </NavigationContainer>
   );
